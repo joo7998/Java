2ac8b33d31c3d96d35d0ea9e36bcfb7ea16096d9
MJ <joo7998@gmail.com>
2021-05-03 14:18:39 +0900

    example

--- JavaEx/src/com/javaex/basics/ConsoleEx.java
+++ JavaEx/src/com/javaex/basics/ConsoleEx.java
@@ -2,11 +2,37 @@ package com.javaex.basics;
 
 public class ConsoleEx {
 
+import java.util.Scanner; //java.util package의 Scanner class 로드	
+	
 	public static void main(String[] args) {
 		consoleOutputEx();
+		consoleInputEx();
 
 	}
     
+	private static void consoleInputEx() {
+		// Tip.1 : ctrl + shift + O 
+		//     2 : ctrl + space (자동완) 
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("이름을 입력해 주세");
+		System.out.println("이름:");
+		
+		String name = scanner.next();
+		
+		System.out.print("나이:");
+		
+		int age = scanner.nextInt();
+		
+		System.out.println("당신의 이름은 " + name + "이고 " + age + "살입니다.");
+		
+		// 쓰고나면 닫아주기
+		scanner.close();
+		
+		
+	}
+
 	private static void consoleOutputEx() {
 		
 		// print 메서드 : 출력후 개행 안함
@@ -26,4 +52,7 @@ public class ConsoleEx {
         System.err.println("표준 에러 출력!");
 		
 	}
+	
+	
+	
 }
