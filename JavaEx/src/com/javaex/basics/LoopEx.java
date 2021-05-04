bcc98213319325fe33f4f3e363ce4142c312832f
MJ <joo7998@gmail.com>
2021-05-04 10:14:32 +0900

    loopex

--- JavaEx/src/com/javaex/basics/LoopEx.java
+++ JavaEx/src/com/javaex/basics/LoopEx.java
@@ -9,7 +9,46 @@ public class LoopEx {
 //		whileGugu();
 //		whileInfinity();
 		doWhileEx();
+		forLoopEx();
+		continueEx();
+		breakEx();
 	}
+	
+    private static void breakEx(){
+    	//6, 14로 동시에 나눠지는 수, 어디까지 루프 돌려야?, while 적합
+    	int num = 1;
+    	
+    	while(true) {
+    		if (num % 6 ==0 && num % 14 == 0) break;
+    		num ++;
+    	}
+    	System.out.println("\n" + num);
+    }
+    
+	
+	private static void continueEx() {
+		// 1 ~ 20 중 2, 3의 배수 제외 나머지 출력
+		for (int i = 1; i <= 20; i++) {
+			if (i % 2 == 0 || i % 3 == 0) continue; // 2,3 배수면 뒤코드 실행 안, 다음 루
+			
+			System.out.println(i);
+		}
+		
+	}
+	
+    private static void forLoopEx() {
+    	// 구구단 출력 for 문 버전 (while 문과 비교)
+    	Scanner scanner = new Scanner(System.in);
+    	
+    	System.out.print("단을 입력하세요:");
+    	int dan = scanner.nextInt();
+    	
+    	// 형식 : for(제어변수초기화; 반복조건체크; 증감식) 
+    	for (int num = 1; num <= 9; num++) {
+    		System.out.println(dan + "*" + num + "=" + (dan * num));
+    	}
+    	scanner.close();
+    }
 	
 	private static void doWhileEx() {
 		//	while 문의 반복 조건을 뒤에서 확인
