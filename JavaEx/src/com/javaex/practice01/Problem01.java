5ebf3adadcf90a812ec1e5603db73c91d6a7c22e
MJ <joo7998@gmail.com>
2021-05-05 14:57:10 +0900

    compiled

--- JavaEx/src/com/javaex/practice01/Problem01.java
+++ JavaEx/src/com/javaex/practice01/Problem01.java
@@ -6,7 +6,7 @@ public class Problem01 {
 	
 	public static void main(String[] args) {
 		printStars();
-		forLoopEx();
+		forMultiplicationLoop();
 		problem01();
 		problem02();
 	}
@@ -22,8 +22,7 @@ public class Problem01 {
 		}
 	}
 
-
-	private static void forLoopEx() {
+	private static void forMultiplicationLoop() {
 		for (int i=2; i<10; i++) {
 			System.out.println();
 		
@@ -33,16 +32,12 @@ public class Problem01 {
 		}
 	}
 	
-	
 	private static void problem01() {
 		for (int i = 1; i < 100; i++) {
 			if (i % 5 == 0 && i % 7 == 0)	//continue; (배수 제외값들 모두 출력)
 			System.out.println(i);   
 		}
 	}
-	 
-	 
-	 
 	 
 	private static void problem02() {
 		Scanner scanner = new Scanner(System.in);
