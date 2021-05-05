54cab71a0ba9c49d2e579339f4111696450bd2d3
MJ <joo7998@gmail.com>
2021-05-05 14:56:59 +0900

    ex complied into one file

--- JavaEx/src/com/javaex/basics/ForMultiplictionTable.java
+++ JavaEx/src/com/javaex/basics/ForMultiplictionTable.java
@@ -10,7 +10,7 @@ public class ForMultiplictionTable {
 	private static void printStars() {
 		int i,j;
 		for (i=0; i<7; i++) {
-			System.out.println("\n");
+			System.out.println();
 			
 			for (j=1; j<1+i; j++) {
 				System.out.print("*");
@@ -22,7 +22,7 @@ public class ForMultiplictionTable {
 
 	private static void forLoopEx() {
 		for (int i=2; i<10; i++) {
-			System.out.println("\n");
+			System.out.println();
 		
 		    for (int m=1; m<10; m++) {
 				System.out.println(i + "*" + m + " = " + (i*m));
--- JavaEx/src/com/javaex/practice01/Problem01.java
+++ JavaEx/src/com/javaex/practice01/Problem01.java
@@ -4,40 +4,59 @@ import java.util.Scanner;
 
 public class Problem01 {
 	
-	
 	public static void main(String[] args) {
+		printStars();
+		forLoopEx();
 		problem01();
 		problem02();
 	}
 		
-	 private static void problem01() {
+	private static void printStars() {
+		int i,j;
+		for (i=0; i<7; i++) {
+			System.out.println();
+			
+			for (j=1; j<1+i; j++) {
+				System.out.print("*");
+			}
+		}
+	}
+
+
+	private static void forLoopEx() {
+		for (int i=2; i<10; i++) {
+			System.out.println();
+		
+		    for (int m=1; m<10; m++) {
+				System.out.println(i + "*" + m + " = " + (i*m));
+			}
+		}
+	}
+	
+	
+	private static void problem01() {
 		for (int i = 1; i < 100; i++) {
 			if (i % 5 == 0 && i % 7 == 0)	//continue; (배수 제외값들 모두 출력)
 			System.out.println(i);   
 		}
 	}
 	 
+	 
+	 
+	 
 	private static void problem02() {
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("숫자를 입력하세요:");
-		int n = scanner.nextInt();		
-		for (int x = 1; y<=n; x++) {
-			for (int y=0; y<x; y++)
-				System.out.print
+		int n = scanner.nextInt();	
+		
+		for (int x = 1; x<=n; x++) {
+			String result = "";
+			for (int y=0; y<x; y++) {
+				result += x;}
+			System.out.println(result);
 		}
 		
 	}
 	
-	private static void forLoopEx()
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("단을 입력하세요:");
-		int dan = scanner.nextInt();
-		for (int num = 1; num <= 9; num++) {
-			System.out.println(dan + "*" + num + "=" + (dan * num));
-		}
-		scanner.close();
-	}
-
-
 }
