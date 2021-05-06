90e677b53bfa18edddf2eddfc069f4aa9a756eac
MJ <joo7998@gmail.com>
2021-05-06 13:53:24 +0900

    getter setter

--- JavaEx/src/com/javaex/oop/goods/v1/GoodsApp.java
+++ JavaEx/src/com/javaex/oop/goods/v1/GoodsApp.java
@@ -5,15 +5,18 @@ public class GoodsApp {
 	public static void main(String[] args) {
 		Goods notebook = new Goods();
 		notebook.name = "LG Gram";
-		notebook.price = 15000;
+		notebook.price = 1500000;
 		
 		Goods smartphone = new Goods();
 		smartphone.name = "iPhone";
 		smartphone.price = 1000000;
 		
-		System.out.printf("%s, %d원%n", notebook.name, notebook.price);
-		System.out.printf("%s, %d원%n", smartphone.name, smartphone.price);
-
+		System.out.printf("%s, %d원%n", 
+				notebook.name,
+				notebook.price);
+		System.out.printf("%s, %d원%n", 
+				smartphone.name,
+				smartphone.price);
 	}
 
 }
--- JavaEx/src/com/javaex/oop/goods/v2/Goods.java
+++ JavaEx/src/com/javaex/oop/goods/v2/Goods.java
@@ -1,10 +1,34 @@
 package com.javaex.oop.goods.v2;
 
 // v2. 정보 은닉
+//     정보에 접근할 수 없는 특수 method : getter , setter 
 
 public class Goods {
 	// 필드
    private String name;
    private int price;
 	
+//	Getter/Setter
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	//	메서드
+	public void showInfo() {
+		System.out.println("상품명:" + name);
+		System.out.println("가격:" + price);
+	}
+
 }
